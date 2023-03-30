61030c02be8daccd21fc989468a75741853f94b3
luoisz <86519689+luoisz@users.noreply.github.com>
2023-03-30 09:42:23 +0630

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,16 +3,14 @@ import json
 from distutils.util import strtobool as stb
 
 # --------------------------------------
-BOT_TOKEN = ""
-GDRIVE_FOLDER_ID = ""
+GDRIVE_FOLDER_ID = "0ADWS1XPq6WKnUk9PVA"
 # Default folder id.
-OWNER_ID = 123455673
 # Example: OWNER_ID = 619418070
-AUTHORISED_USERS = []
+AUTHORISED_USERS = [123,456,789]
 # Example: AUTHORISED_USERS = [63055333, 100483029, -1003943959]
 INDEX_URL = ""
 IS_TEAM_DRIVE = True
-USE_SERVICE_ACCOUNTS = True
+USE_SERVICE_ACCOUNTS = False
 # --------------------------------------
 
 # dont edit below this >
